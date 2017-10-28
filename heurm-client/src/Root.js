8eda66793d817e09454847600e7e86da56e71450
이진석 <sonaky47@gmail.com>
2017-10-28 20:53:50 +0900

    heurm front 도입 store, route, root, redux

---
+++ heurm-client/src/Root.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+
+class Root extends Component {
+    render () {
+        const { store } = this.props;
+        return (
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Route path="/" component={App} />
+                </BrowserRouter>
+            </Provider>
+        )
+    }
+}
+export default Root;
